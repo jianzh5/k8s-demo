8c932201e65b7a4ef323445c103c6b7c7d6d1027
jam <jianzh5@163.com>
2022-08-15 22:53:12 +0800

    优化代码结构

---
+++ easy-demo/src/main/java/com/jianzh5/demo/Demo1Application.java
@@ -0,0 +1,24 @@
+package com.jianzh5.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jam
+ */
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "hello,javadaily";
+    }
+
+}
