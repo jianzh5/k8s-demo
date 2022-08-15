d5b07373f56ab57a31588f31233ab6082776313a
jam <jianzh5@163.com>
2022-08-15 22:16:25 +0800

    Initial commit

---
+++ src/main/java/com/jianzh5/k8s/K8sDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jianzh5.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jianzh5/k8s/K8sDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianzh5.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
