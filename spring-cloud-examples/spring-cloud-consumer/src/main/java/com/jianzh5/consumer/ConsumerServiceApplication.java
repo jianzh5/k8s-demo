e938d5384d5b85765d79690a7b35a183844e9460
jam <jianzh5@163.com>
2022-09-01 18:49:19 +0800

    集成nacos注册中心

--- spring-cloud-examples/spring-cloud-consumer/src/main/java/com/jianzh5/consumer/ConsumerServiceApplication.java
+++ spring-cloud-examples/spring-cloud-consumer/src/main/java/com/jianzh5/consumer/ConsumerServiceApplication.java
@@ -2,6 +2,7 @@ package com.jianzh5.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author jam
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/8/31 22:08
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerServiceApplication.class, args);
--- spring-cloud-examples/spring-cloud-gateway/src/main/java/com/jianzh5/gateway/GatewayApplication.java
+++ spring-cloud-examples/spring-cloud-gateway/src/main/java/com/jianzh5/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.jianzh5.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author jam
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/8/31 22:08
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
